ecfdd50df64dcefc8dbf44eeedd7096bac0346a8
Ulf Liedberg <ulf@alfhild.io>
2017-08-17 16:15:05 +0200

    actual parent-child testing

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # etags
 
+* add `sub.child.com` & a couple of parent domains in `/etc/hosts`
+
     > npm install
     > npm run dev | ./node_modules/bunyan/bin/bunyan
---
+++ src/koalogger.js
@@ -0,0 +1,31 @@
+// taken from https://github.com/ivpusic/koa-bunyan plus some extra logs for host header
+
+const util = require('util');
+
+module.exports = function (logger, opts) {
+  opts = opts || {};
+
+  const defaultLevel = opts.level || 'info';
+  const requestTimeLevel = opts.timeLimit;
+
+  return function (ctx, next) {
+    const startTime = new Date().getTime();
+    logger[defaultLevel](util.format('[REQ] %s %s %s', ctx.method, ctx.host, ctx.url));
+
+    const done = function () {
+      const requestTime = new Date().getTime() - startTime;
+      const localLevel = defaultLevel;
+
+      if (requestTimeLevel && requestTime > requestTimeLevel) {
+        localLevel = 'warn';
+      }
+      logger[localLevel](util.format('[RES] %s %s %s (%s) took %s ms', ctx.method, ctx.host, ctx.originalUrl, ctx.status, requestTime));
+    };
+
+    ctx.res.once('finish', done);
+    ctx.res.once('close', done);
+
+
+    return next();
+  };
+};
